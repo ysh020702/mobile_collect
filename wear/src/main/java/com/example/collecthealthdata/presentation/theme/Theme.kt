7e70ccbb7d9897397b250fe6c9ae471613e25de8
ysh020702 <ysh020702@gmail.com>
2025-03-31 04:59:19 +0900

    message

---
+++ wear/src/main/java/com/example/collecthealthdata/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.collecthealthdata.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun CollectHealthDataTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
