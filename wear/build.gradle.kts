70fa28d86c93a74dd6a40f7558e3f54383ccdd38
ysh020702 <ysh020702@gmail.com>
2025-04-01 09:26:43 +0900

    안드로이드 그래들 복붙 누가했냐;

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -62,5 +62,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     debugImplementation(libs.androidx.tiles.tooling)
-    wearApp(project(":wear"))
 }
